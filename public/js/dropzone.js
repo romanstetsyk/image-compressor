0bb281d46c6622f8749de97fdec58cbba5de0563
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-09-16 00:20:30 +0100

    remove unused ref and comments

--- public/js/dropzone.js
+++ public/js/dropzone.js
@@ -29,7 +29,6 @@ let myDropzone = new Dropzone("#my-dropzone", {
       dialog: previewElement.querySelector("dialog"),
       openDialogBtn: previewElement.querySelector(".compare"),
       changeQualityForm: previewElement.querySelector(".changeQuality"),
-      dialogControlForm: previewElement.querySelector(".dialogControl"),
       saveDialogBtn: previewElement.querySelector(".save"),
       closeDialogBtn: previewElement.querySelector(".close"),
       img2: previewElement.querySelector("[data-img2]"),
@@ -88,37 +87,5 @@ let myDropzone = new Dropzone("#my-dropzone", {
       sizeFromForm = data.jpegInfo.size;
       refs.img2.src = data.jpegBase64;
     });
-
-    // const comparisonsDiv = document.querySelector("#comparisons");
-    // comparisonsDiv.insertAdjacentHTML("beforeend", response.html);
-    // const sliderContainer = comparisonsDiv.querySelector(
-    //   `[data-id="${response.src}"]`
-    // );
-    // const form = sliderContainer.querySelector("form");
-    // form.addEventListener("submit", async e => {
-    //   e.preventDefault();
-    //   const inp = form.querySelector("input");
-    //   const res = await fetch("/optimize", {
-    //     method: "post",
-    //     headers: { "Content-type": "application/json" },
-    //     body: JSON.stringify({
-    //       dataId: sliderContainer.dataset.id,
-    //       quality: inp.value,
-    //     }),
-    //   });
-    //   const data = await res.json();
-    //   console.log(data);
-    //   sliderContainer.querySelector("[data-img2").src = data.jpegBase64;
-    //   sliderContainer.querySelector("[data-newSizeJpeg]").innerHTML = (
-    //     data.jpegInfo.size / 1024
-    //   ).toFixed(2);
-    //   sliderContainer.querySelector("[data-newSizeWebp]").innerHTML = (
-    //     data.webpInfo.size / 1024
-    //   ).toFixed(2);
-    //   sliderContainer.querySelector("[data-downloadJpeg]").href =
-    //     data.jpegBase64;
-    //   sliderContainer.querySelector("[data-downloadWebp]").href =
-    //     data.webpBase64;
-    // });
   },
 });
