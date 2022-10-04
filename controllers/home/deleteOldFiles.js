50a237ce25253412397a01c5424dd7739867f3c7
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-10-04 16:58:18 +0100

    change cron job interval to 30 minutes

--- controllers/home/deleteOldFiles.js
+++ controllers/home/deleteOldFiles.js
@@ -12,9 +12,9 @@ const deleteOldFiles = async (req, res) => {
     }
 
     const images = await Image.find({
-      // files created earlier than 20 minutes ago
       // the shortest interval the task can run in Github Actions is every 5 min
-      createdAt: { $lt: new Date(Date.now() - 20 * 60 * 1000) },
+      // files created earlier than 30 minutes ago
+      createdAt: { $lt: new Date(Date.now() - 30 * 60 * 1000) },
     });
     images.forEach(async img => {
       // Delete from fs
